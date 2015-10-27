835fec0edf27b0015c8ee744b1844d8e9390ed80
brandon in <brandonin@me.com>
2015-10-27 09:01:16 -0700

    my first Init

---
+++ application/controllers/main.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class main extends CI_Controller {
+
+	public function __construct(){
+			parent::__construct();
+			// $this->output->enable_profiler(TRUE);
+	}
+
+	public function index()
+	{
+		$this->load->view('dashboard');
+	}
+}
---
+++ application/views/dashboard.php
@@ -0,0 +1,358 @@
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title></title>
+	<link rel="stylesheet" href="http://cdn.foundation5.zurb.com/foundation.css">
+	<link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.5.7/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.5.8/slick-theme.css"/>
+	<script type="text/javascript">
+	</script>
+	<style type="text/css">
+	.slider{
+		width:1000px;
+		height: 450px;
+		margin:20px auto;
+		/*background-color: white;*/
+		text-align: center;
+	}
+	.slick-slide {
+		border-left: solid blue 10px;
+		text-align: center;
+		vertical-align: center;
+
+	}
+/*	.products {
+		height:300px;
+		width:300px;
+	}
+*/	
+	.auth-plain {
+	  padding-right: 0px;
+	  padding-left: 0px;
+	  padding-top: 0px;
+	}
+
+	.left-solid {
+	margin-top: 0px;
+	margin-bottom: 0px;
+	}
+
+	.signup-panel {
+	  border-radius: 5px;
+	  padding: 15px;
+	  margin-top: 30px;
+	  margin-bottom: 30px;
+	  background: #fff;
+	}
+
+	.signup-panel a{
+	  color: #fff;
+	}
+
+	.signup-panel i {
+	  font-size: 30px;
+	  line-height: 50px;
+	  color: #999;
+	}
+	.signup-panel form input, .signup-panel form span {
+	  height: 50px;
+	}
+	.signup-panel .welcome {
+	  font-size: 26px;
+	  text-align: center;
+	  margin-left: 0;
+	}
+	.signup-panel p {
+	  font-size: 13px;
+	  font-weight: 200;
+	  text-align: center;
+	}
+	.signup-panel .button {
+	  margin-left: 35%;
+	}
+
+	.newusers {
+	  background: #fff;
+	}
+</style>
+</head>
+<body>
+<div class="row">
+        <div class="large-3 columns">
+          <h1><img src="http://placehold.it/400x100&text=Logo"/></h1>
+        </div>
+        <div class="large-9 columns">
+          <ul class="right button-group">
+          <li><a href="#" class="button">Link 1</a></li>
+          <li><a href="#" class="button">Link 2</a></li>
+          <li><a href="#" class="button paulund_modal_3">Log In</a></li>
+          <li><a href="#" class="button paulund_modal_2">Register</a></li>
+          </ul>
+         </div>
+       </div>
+      
+     
+     
+     
+     
+   	<!-- Slick slider -->
+	<div class="slider">
+		<div class="slick-slide">
+			<div class="feature">
+				<div class="wishlist">
+				<div class="share-buttons">
+					<span class="share-facebook">
+					</span>
+				</div>
+				</div>
+			</div>
+			<div class="feature">
+				<div class="wishlist">
+				<div class="share-buttons">
+					<span class="share-twitter">
+					</span>
+				</div>
+				</div>
+			</div>
+			<div class="feature">
+				<div class="wishlist">
+				<div class="share-buttons">
+					<span class="share-pinterest">
+					</span>
+				</div>
+				</div>
+			</div>
+			<h3>
+	  			<img src="/user_guide/_images/beard.jpg">
+			</h3>
+		</div>
+		<div class="slick-slide">
+				<div class="wishlist">
+				<div class="share-buttons">
+					<span class="share-facebook">
+					</span>
+				</div>
+				</div>
+			<div class="feature">
+				<div class="wishlist">
+				<div class="share-buttons">
+					<span class="share-twitter">
+					</span>
+				</div>
+				</div>
+			</div>
+			<div class="feature">
+				<div class="wishlist">
+				<div class="share-buttons">
+					<span class="share-pinterest">
+					</span>
+				</div>
+				</div>
+			</div>
+	  		<h3>
+		  		<img src="/user_guide/_images/soap.jpg">
+			</h3>
+		</div>
+	  <div class="slick-slide">
+				<div class="wishlist">
+				<div class="share-buttons">
+					<span class="share-facebook">
+					</span>
+				</div>
+				</div>
+			<div class="feature">
+				<div class="wishlist">
+				<div class="share-buttons">
+					<span class="share-twitter">
+					</span>
+				</div>
+				</div>
+			</div>
+			<div class="feature">
+				<div class="wishlist">
+				<div class="share-buttons">
+					<span class="share-pinterest">
+					</span>
+				</div>
+				</div>
+			</div>
+		  	<h3>
+		  		<img src="/user_guide/_images/adapter.jpg">
+		  	</h3>
+		</div>
+		  <div class="slick-slide">
+					<div class="wishlist">
+					<div class="share-buttons">
+						<span class="share-facebook">
+						</span>
+					</div>
+					</div>
+				<div class="feature">
+					<div class="wishlist">
+					<div class="share-buttons">
+						<span class="share-twitter">
+						</span>
+					</div>
+					</div>
+				</div>
+				<div class="feature">
+					<div class="wishlist">
+					<div class="share-buttons">
+						<span class="share-pinterest">
+						</span>
+					</div>
+					</div>
+				</div>
+			<h3>
+				<img src="/user_guide/_images/swiss.jpg">
+			</h3>
+		</div>
+		  <div class="slick-slide">
+					<div class="wishlist">
+					<div class="share-buttons">
+						<span class="share-facebook">
+						</span>
+					</div>
+					</div>
+				<div class="feature">
+					<div class="wishlist">
+					<div class="share-buttons">
+						<span class="share-twitter">
+						</span>
+					</div>
+					</div>
+				</div>
+				<div class="feature">
+					<div class="wishlist">
+					<div class="share-buttons">
+						<span class="share-pinterest">
+						</span>
+					</div>
+					</div>
+				</div>
+		  	<h3>
+		  		<img src="/user_guide/_images/yoga.jpg">
+		  	</h3>
+		</div>
+	  <!-- <div class="uno"><h3><img src="#"></h3></div> -->
+	</div>
+	<!-- end slick slider -->
+
+	<div class="products col sm6">
+
+	</div>
+      
+     
+     
+      <div class="row">
+        <div class="large-4 columns">
+          <img src="http://placehold.it/400x300&text=[img]"/>
+          <h4>This is a content section.</h4>
+          <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+        </div>
+        
+        <div class="large-4 columns">
+          <img src="http://placehold.it/400x300&text=[img]"/>
+          <h4>This is a content section.</h4>
+          <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+        </div>
+        
+        <div class="large-4 columns">
+          <img src="http://placehold.it/400x300&text=[img]"/>
+          <h4>This is a content section.</h4>
+          <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+        </div>
+      
+        </div>
+        
+     
+    <div class="row">
+        <div class="large-12 columns">
+        
+          <div class="panel">
+            <h4>Get in touch!</h4>
+                
+            <div class="row">
+              <div class="large-9 columns">
+                <p>We'd love to hear from you, you attractive person you.</p>
+              </div>
+              <div class="large-3 columns">
+                <a href="#" class="radius button right">Contact Us</a>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+     
+       
+      
+      <footer class="row">
+        <div class="large-12 columns">
+          <hr/>
+          <div class="row">
+            <div class="large-6 columns">
+              <p>© Copyright no one at all. Go to town.</p>
+            </div>
+            <div class="large-6 columns">
+              <ul class="inline-list right">
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+                <li><a href="#">Link 4</a></li>
+              </ul>
+            </div>
+          </div>
+        </div> 
+      </footer>
+    
+	<!-- end log in button -->
+
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.js"></script>
+<script type="text/javascript" src="http://cdn.foundation5.zurb.com/foundation.js"></script>
+<script src="/user_guide/_static/js/jquery.paulund_modal_box.js"></script>
+<script type="text/javascript" src="http://cdn.jsdelivr.net/jquery.slick/1.5.8/slick.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+	$('.slider').slick({
+		infinite: true,
+		slidesToShow: 2,
+		centerMode: true,
+		centerPadding: '60px',
+		autoplay: true,
+		autoplaySpeed: 3000,
+		responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        arrows: false,
+	        centerMode: true,
+	        centerPadding: '40px',
+	        slidesToShow: 3
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        arrows: false,
+	        centerMode: true,
+	        centerPadding: '40px',
+	        slidesToShow: 1
+	      }
+	    }
+	  ]
+	});
+	$('.paulund_modal').paulund_modal_box();
+	$('.paulund_modal_2').paulund_modal_box({
+		title:'Register',
+		description:"<input type='text' value='hi'>"
+	});
+	$('.paulund_modal_3').paulund_modal_box({
+		title:'Sign In'
+	});
+
+	});
+</script>
+</body>
+</html>
